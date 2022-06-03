784088df1b39b4c6d6e1f779a1750c94ed2869cf
Houssemeddine Souissi <devBrains404@gmail.com>
2022-06-03 20:23:16 +0100

    feat: Serializable Model Task

--- lib/model/Task.dart
+++ lib/model/Task.dart
@@ -5,4 +5,22 @@ class Task{
   DateTime deadline;
   Task(this.title,this.status,this.description,this.deadline);
 
+
+  Map<String,dynamic> toJson(){
+    return {
+      "title":title,
+      "status":status,
+      "description":description,
+      "deadline":deadline.toIso8601String(),
+    };
+  }
+
+  Task.fromJson(Map<String,dynamic> json):
+        title=json['title'],
+        status=json['status'],
+        description=json['description'],
+        deadline=DateTime.tryParse(json['deadline'])!;
+
+
+
 }
