d079b3ada6c6d59d02f5c3980809da9d7c53a390
Houssemeddine Souissi <devBrains404@gmail.com>
2022-07-23 13:16:47 +0100

    ref: reformat code

--- lib/provider/TaskModel.dart
+++ lib/provider/TaskModel.dart
@@ -15,45 +15,52 @@ class TaskModel extends ChangeNotifier {
     globals.later:[Task("Task 1",false,"Create Provider",DateTime.now().add(Duration(days: 1)))],
   };
 
-  Map<String,List<Task>> get todoTasks => _todoTasks;
+  Map<String, List<Task>> get todoTasks => _todoTasks;
 
-  void add(Task _task){
+  void add(Task _task) {
     String _key = guessTodoKeyFromDate(_task.deadline);
-    if(_todoTasks.containsKey(_key)){
+    if (_todoTasks.containsKey(_key)) {
       _todoTasks[_key]!.add(_task);
       notifyListeners();
     }
-
-
   }
+
   int countTasksByDate(DateTime _datetime) {
     String _key = guessTodoKeyFromDate(_datetime);
-    if(_todoTasks.containsKey(_key)){
-      _todoTasks[_key]!.where((task) =>
-      task.deadline.day== _datetime.day &&
-          task.deadline.month== _datetime.month &&
-          task.deadline.year== _datetime.year
-      ).length;
+    if (_todoTasks.containsKey(_key)) {
+      _todoTasks[_key]!
+          .where((task) =>
+              task.deadline.day == _datetime.day &&
+              task.deadline.month == _datetime.month &&
+              task.deadline.year == _datetime.year)
+          .length;
     }
-    return 0 ;
+    return 0;
   }
 
-  void markAsDone(String key,int index){
-    _todoTasks[key]![index].status=true;
+  void markAsDone(String key, int index) {
+    _todoTasks[key]![index].status = true;
     notifyListeners();
   }
 
   String guessTodoKeyFromDate(DateTime deadline) {
-    if(deadline.isPast && !deadline.isToday){return globals.late;}
-    //TODO need Test (next year)
-    else if (deadline.isToday){return globals.today;}
-    //TODO need Test (next year)
-    else if (deadline.isTomorrow){return globals.tomorrow;}
-    else if (deadline.getWeek==DateTime.now().getWeek && deadline.year==DateTime.now().year){return globals.thisWeek;}
-    else if (deadline.getWeek==DateTime.now().getWeek+1 && deadline.year==DateTime.now().year){return globals.nextWeek;}
-    //TODO need Test (next year)
-    else if (deadline.isThisMonth){return globals.thisMonth;}
-    else{return globals.later;}
+    if (deadline.isPast && !deadline.isToday) {
+      return globals.late;
+    } else if (deadline.isToday) {
+      return globals.today;
+    } else if (deadline.isTomorrow) {
+      return globals.tomorrow;
+    } else if (deadline.getWeek == DateTime.now().getWeek &&
+        deadline.year == DateTime.now().year) {
+      return globals.thisWeek;
+    } else if (deadline.getWeek == DateTime.now().getWeek + 1 &&
+        deadline.year == DateTime.now().year) {
+      return globals.nextWeek;
+    } else if (deadline.isThisMonth) {
+      return globals.thisMonth;
+    } else {
+      return globals.later;
+    }
   }
 
 }
